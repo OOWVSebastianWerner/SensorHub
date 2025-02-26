8cb3e58e096c60a9cb38298ddb7124636472de22
Ivo Sieghold <i.sieghold@oowv.de>
2025-02-26 09:53:38 +0100

    keine Ahnung

--- py/service_dwd_measurements.py
+++ py/service_dwd_measurements.py
@@ -1,5 +1,7 @@
 
 #%%
+import csv
+import json
 import requests
 from pathlib import Path
 import pandas as pd
@@ -11,26 +13,24 @@ import os
 import time
 
 
-# Alle Stations ID's abrufen vom Frostserver
+###############  DOWNLOAD of the .txt files from the DWD-Server ##################
+
+
 #%%
 
 base_url = "https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/hourly/precipitation/recent/"
 output_dir = r'C:\Users\User\Desktop\stundendaten'
 
-stations = [
-    '00078',  
-    '00053',    
-]
 
-qry_ids = (
-    f"{config.endpoints['things']}"
-    "?$filter=properties/station_type eq 'raing gauge'"
-    "&$select=@iot.id,properties/foreign_id"
-)
-
+## Get the staions for which we want to download data
 
 #%%
 
+qry_ids = (
+    f"{config.endpoints['things']}"
+    "?$filter=properties/station_type eq 'raingauge_station'"
+    "&$select=@iot.id,properties/foreign_id"
+)
 with requests.Session() as session:
     # Set global header
     session.headers.update({'Content-Type': 'application/json; charset=UTF-8'})
@@ -42,19 +42,20 @@ with requests.Session() as session:
     # as long as there is '@iot.nextLink' present in things, do another request 
     # and combine it with things
     while '@iot.nextLink' in things.keys():
-        things = things | session.get(things['@iot.nextLink']).json()
-        things.pop('@iot.nextLink')
+        
+        nextLink = things['@iot.nextLink']
+        next_things = session.get(nextLink).json()
 
-    for thing in things['value']:
-        id_ = thing['@iot.id']
-        uuid = thing['properties']['foreign_id']
+        things['value'] += next_things['value']
+
+        if '@iot.nextLink' in next_things.keys():
+            things['@iot.nextLink'] = next_things['@iot.nextLink']
+        else:
+            things.pop('@iot.nextLink')
 
 #%%
-print(things)
-
-#%%
-def download_and_unzip_file(station_id):
-    file_name = f"stundenwerte_RR_{station_id}_akt.zip"
+def download_and_unzip_file(thing_id):
+    file_name = f"stundenwerte_RR_{str(thing_id).zfill(5)}_akt.zip"
     url = base_url + file_name
 
     try:
@@ -66,8 +67,8 @@ def download_and_unzip_file(station_id):
 
         # Zip-Datei in den Arbeitsspeicher laden
         with zipfile.ZipFile(io.BytesIO(response.content)) as z:
-            z.extractall(os.path.join(output_dir, station_id))
-            print(f"Extracted {file_name} to {output_dir}/{station_id}")
+            z.extractall(os.path.join(output_dir, str(thing_id).zfill(5)))
+            print(f"Extracted {file_name} to {output_dir}/{thing_id}")
 
     except requests.exceptions.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")  # HTTP Fehler anzeigen
@@ -75,135 +76,139 @@ def download_and_unzip_file(station_id):
         print(f"Other error occurred: {err}")  # Andere Fehler anzeigen
 
 def main():
-    while True:
-        for station in stations:
-            download_and_unzip_file(station)
+    for i in things['value']:
+        download_and_unzip_file(i['properties']['foreign_id'])
         
-        # Warte eine Minute
-        print("Waiting for the next cycle...")
-        time.sleep(60)
+
 
 if __name__ == "__main__":
-    main()
+   main()
 
 #%%
 
-# with requests.Session() as session:
-#     # Set global header
-#     session.headers.update({'Content-Type': 'application/json; charset=UTF-8'})
+############### IMPORT of the DATA to the Frost-Server  #######################
 
-#     # get things from FROST-Server
-#     things = session.get(qry_ids).json()
+
+#def get_data():
+#%%
+for folder in os.listdir(output_dir):
+    folder_path = os.path.join(output_dir, folder)
     
-#     # FROST-Server limits requests to 100 entries by default
-#     # as long as there is '@iot.nextLink' present in things, do another request 
-#     # and combine it with things
-#     while '@iot.nextLink' in things.keys():
-#         things = things | session.get(things['@iot.nextLink']).json()
-#         things.pop('@iot.nextLink')
+    # Überprüfen, ob es ein Verzeichnis mit 5-stelliger Struktur ist
+    if os.path.isdir(folder_path) and folder.isdigit() and len(folder) == 5:
+        print(f"Processing folder: {folder}")
 
-#     for thing in things['value']:
-#         id_ = thing['@iot.id']
-#         uuid = thing['properties']['foreign_id']
+        txt_files = [f for f in os.listdir(folder_path) if f.startswith('produkt_rr_stunde') and f.endswith('.txt')]
 
+        if txt_files:
+# Falls mehrere Dateien passen, z.B. bei historischen Daten, nehmen wir die neuste (nach Alphabet sortiert)
+            txt_files.sort()
+            txt_file_path = os.path.join(folder_path, txt_files[-1])  # Die letzte in der sortierten Liste
+            print(f"Reading data from {txt_file_path}")
 
+            # Datei öffnen und einlesen
+            with open(txt_file_path, 'r', encoding='latin1') as file:
+                reader = csv.reader(file, delimiter=';')
+                rows = list(reader)
 
+                # Header und Metadaten überspringen
+                data_rows = rows[:20]
+                
+                # Nur die letzten 12 Zeilen
+                last_12_rows = data_rows[-12:]
 
+                print(f"Last 12 entries for station {folder}:")
+                for row in last_12_rows:
+                    print(row)
+        else:
+            print(f"No .txt file found in {folder_path}")
 
+#%%
 
+json_data=[]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# qry_things = (
-#     f"{config.endpoints['things']}"
-#     "?$filter=properties/station_type eq 'raingauge_station'"
-#     "&$select=@iot.id,properties/foreign_id"
-#     "&$expand=Datastreams($select=@iot.id,@iot.selfLink)"
-# )
-
-# # POST-function
-# def post_observations(session, datastream, data):
-#     responses = []
-#     for _, row in data.iterrows():
-#         row_dict = row.to_dict()
-#         response = session.post(f'{datastream}/Observations', json=row_dict)
-#     responses.append((response.status_code, response.text))
-#     return responses
-
-
-
-
-
-
-# with requests.Session() as session:
-#     # Set global header
-#     session.headers.update({'Content-Type': 'application/json; charset=UTF-8'})
-
-#     # get things from FROST-Server
-#     things = session.get(qry_things).json()
+# Datum und Wert aus den letzten 12 Zeilen extrahieren und in JSON umwandeln
+for row in last_12_rows:
+    datum = row[1].strip()  # MESS_DATUM
+    wert = row[3].strip()   # RS
     
-#     # FROST-Server limits requests to 100 entries by default
-#     # as long as there is '@iot.nextLink' present in things, do another request 
-#     # and combine it with things
-#     while '@iot.nextLink' in things.keys():
+    # JSON-Format anpassen
+    json_entry = {
+        "phenomenonTime": datum,
+        "result": wert
+    }
+    json_data.append(json_entry)
+
+#print(json_data)
+# Ausgabe der JSON-Daten
+json_output = json.dumps(json_data, indent=4)
+#print(json_output)
+
+
+
+#### POST da data
+
+
+qry_things = (
+    f"{config.endpoints['things']}"
+    "?$filter=properties/station_type eq 'raingauge_station'"
+    "&$select=@iot.id,properties/foreign_id"
+    "&$expand=Datastreams($select=@iot.id,@iot.selfLink)"
+)
+
+# POST-function
+def post_observations(session, datastream, data):
+    responses = []
+    for _, row in data.iterrows():
+        row_dict = row.to_dict()
+        response = session.post(f'{datastream}/Observations', json=row_dict)
+    responses.append((response.status_code, response.text))
+    return responses
+
+
+
+
+#%%
+
+with requests.Session() as session:
+    # Set global header
+    session.headers.update({'Content-Type': 'application/json; charset=UTF-8'})
+
+    # get things from FROST-Server
+    things = session.get(qry_things).json()
+    
+    # FROST-Server limits requests to 100 entries by default
+    # as long as there is '@iot.nextLink' present in things, do another request 
+    # and combine it with things
+    while '@iot.nextLink' in things.keys():
         
-#         nextLink = things['@iot.nextLink']
-#         next_things = session.get(nextLink).json()
+        nextLink = things['@iot.nextLink']
+        next_things = session.get(nextLink).json()
 
-#         things['value'] += next_things['value']
+        things['value'] += next_things['value']
 
-#         if '@iot.nextLink' in next_things.keys():
-#             things['@iot.nextLink'] = next_things['@iot.nextLink']
-#         else:
-#             things.pop('@iot.nextLink')
+        if '@iot.nextLink' in next_things.keys():
+            things['@iot.nextLink'] = next_things['@iot.nextLink']
+        else:
+            things.pop('@iot.nextLink')
 
 
-#     for thing in things['value']:
-#         id_ = thing['@iot.id']
-#         uuid = thing['properties']['foreign_id']
-#         # IMPORTANT: only works when there is only one datastream per thing
-#         # @TODO: add filter/check to get the correct datastream if there is more than one.
-#         datastream = thing['Datastreams'][0]['@iot.selfLink']
+    for thing in things['value']:
+        id_ = thing['@iot.id']
+        uuid = thing['properties']['foreign_id']
+        # IMPORTANT: only works when there is only one datastream per thing
+        # @TODO: add filter/check to get the correct datastream if there is more than one.
+        datastream = thing['Datastreams'][0]['@iot.selfLink']
 
-#         datastream_res = session.get(f'{datastream}').json()
-#         # get last phenomenonTime in Datastream
-#         if 'phenomenonTime' in datastream_res.keys():
-#             lastEntryTime = datastream_res['phenomenonTime'].split('/')[1]
-#         else:
-#             lastEntryTime = None
+        datastream_res = session.get(f'{datastream}').json()
+        # get last phenomenonTime in Datastream
+        if 'phenomenonTime' in datastream_res.keys():
+            lastEntryTime = datastream_res['phenomenonTime'].split('/')[1]
+        else:
+            lastEntryTime = None
 
-#         print(f"Processing Thing ID: {id_}, Start: {datetime.now()}, Import: {lastEntryTime}, Datastream: {datastream}")
-        
-#         response = session.get(f'{baseUrl_wsa}stations/{uuid}/W/measurements.json')
-        
-#         if response.status_code == 200 and response.json():
-#             # create DataFrame
-#             df = pd.DataFrame(response.json())
-#             df.rename(columns={'timestamp': 'phenomenonTime', 'value': 'result'}, inplace=True)
 
-#             if lastEntryTime:
-#                 df_to_post = df[df['phenomenonTime'] > lastEntryTime][-5:]
-#             else:
-#                 df_to_post = df[-5:]
-            
-#             r = post_observations(session, datastream, df_to_post)
-#             print(r)
-# # %%
+
+
 
 # %%
